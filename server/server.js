8d3cf5569b03a5df652502cb40ec2c3e3420955c
aidenmjeda <aidenmjeda@icloud.com>
2025-10-18 22:31:38 +0200

    Finished with the server file

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import userRouter from './routes/userRoutes.js';
 
 
 const app = express();
+
+await connectDB();
 
 //Middleware 
 app.use(cors());
@@ -11,6 +15,9 @@ app.use(express.json());
 
 //Routes
 app.get('/', (req, res) => res.send('Server is Live'));
+app.use('/api/users',  userRouter);
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> {
         console.log(`Server running on port ${PORT}`)
