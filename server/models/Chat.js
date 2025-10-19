371008079507ab4d1c1f9f262f5ccf84d5fb5cf4
aidenmjeda <aidenmjeda@icloud.com>
2025-10-19 14:09:23 +0200

    Finished with the Chat file

---
+++ server/models/Chat.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+    userId : {type:String, ref:'User', required:true},
+    username : {type:String, required:true},
+    name : {type:String, required:true},
+    messages : [{
+        isImage: {type:Boolean, required: true},
+        isPublished: {type:Boolean, default: false},
+        role: {type:String, required: true},
+        content: {type:String, required: true},
+        timestamp: {type:Date, default: Date.now}
+    }],
+}, {timestamps:true})
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+export default Chat;
