ea15491116e717389c1455cd55380cabd0feb41e
aidenmjeda <aidenmjeda@icloud.com>
2025-10-20 21:28:51 +0200

    Worked with the Message Controller file

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -8,7 +8,10 @@ export const textMessageController = async(req, res) => {
     try {
         const userId = req.user._id
         const {chatId, prompt} = req.body
+
         const chat = await Chat.findOne({userId, _id:chatId})
+        chat.messages.push({role:"user", content: prompt, timestamp: Date.now(), isImage: false})
+        
     } catch (error) {
         
     }
