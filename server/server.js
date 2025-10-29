e8b258294a734907cd4b57c67e2917273a6537dc
aidenmjeda <aidenmjeda@icloud.com>
2025-10-29 14:04:21 +0100

    Worked with the user and server file

--- server/server.js
+++ server/server.js
@@ -4,22 +4,24 @@ import cors from 'cors';
 import connectDB from './configs/db.js';
 import userRouter from './routes/userRoutes.js';
 import chatRouter from './routes/chatroutes.js';
-
+import messageRouter from './routes/messageRoutes.js';
 
 const app = express();
 
+// Connect Database
 await connectDB();
 
-//Middleware 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-//Routes
+// Routes
 app.get('/', (req, res) => res.send('Server is Live'));
-app.use('/api/users',  userRouter);
+app.use('/api/users', userRouter);
 app.use('/api/chat', chatRouter);
+app.use('/api/messages', messageRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> {
-        console.log(`Server running on port ${PORT}`)
-    });
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
